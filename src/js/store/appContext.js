49506db19ba0d4c20b4e941150275dadb87bce95
lauramagallanes <pasocenturiontours@gmail.com>
2022-12-11 13:44:03 -0300

    cambios en content-personajes

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -34,7 +34,7 @@ const injectContext = PassedComponent => {
 			state.actions.getListaPersonajes();
             state.actions.getListaPlanetas();
             state.actions.getListaVehiculos();
-            state.actions.validToken();
+          
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
