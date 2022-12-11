5b729b580d220aa01d616eec6855efb613a04a40
lauramagallanes <pasocenturiontours@gmail.com>
2022-12-11 13:41:29 -0300

    cambios en el flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,161 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			listaPersonajes: [],
+            listaPlanetas: [],
+            listaVehiculos: [],
+            detallesPersonaje: {},
+            detallesPlaneta: {},
+            detallesVehiculo: {},
+
+            listaFavoritos: [],
+            classNameFavoritos: "btn btn-outline-light",
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			cambiaClassNameFavoritos: () => {
+                let store = getStore()
+                if (store.classNameFavoritos == "btn btn-outline-light") {
+                    setStore({
+                        classNameFavoritos: "btn btn-light"
+                    })
+                } else {
+                    setStore({
+                        classNameFavoritos: "btn btn-outline-light"
+                    })
+                }
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+            },
+            getListaFavoritos: (favorito) => {
+                let store = getStore()
+                if (store.listaFavoritos.includes(favorito)) {
+                    getActions().eliminarFavorito(favorito)
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+                } else {
+
+                    setStore({
+                        listaFavoritos: [...store.listaFavoritos, favorito],
+
+                    }, )
+
+                }
+            },
+
+
+            eliminarFavorito: (favorito) => {
+                let store = getStore()
+                setStore({
+                    listaFavoritos: store.listaFavoritos.filter((item) => item !== favorito)
+                })
+            },
+
+            getListaPersonajes: async () => {
+                try {
+                    const response = await fetch("https://swapi.dev/api/people/")
+                    const data = await response.json();
+
+                    let personajes = data.results;
+
+                    setStore({
+                        listaPersonajes: personajes
+                    });
+                    // console.log(listaPersonajes);
+
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+
+            getListaPlanetas: async () => {
+                try {
+                    const response = await fetch("https://swapi.dev/api/planets/")
+                    const data = await response.json();
+
+                    let planetas = data.results;
+
+                    setStore({
+                        listaPlanetas: planetas
+                    });
+                    // console.log(listaPersonajes);
+
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+
+            getListaVehiculos: async () => {
+                try {
+                    const response = await fetch("https://swapi.dev/api/vehicles/")
+                    const data = await response.json();
+
+                    let vehiculos = data.results;
+
+                    setStore({
+                        listaVehiculos: vehiculos
+                    });
+                    // console.log(listaPersonajes);
+
+                } catch (err) {
+                    console.log(err);
+                }
+            },
+
+            getDetallePersonaje: async (id) => {
+
+                try {
+                    const response = await fetch("https://swapi.dev/api/people/" + id)
+                    const data = await response.json();
+                    console.log(data);
+                    // let propiedades = data.results;
+                    // console.log(propiedades);
+                    setStore({
+                        detallesPersonaje: data
+                    });
+
+
+                } catch (err) {
+                    console.log(err);
+                }
+
+            },
+
+            getDetallePlaneta: async (id) => {
+
+                try {
+                    const response = await fetch("https://swapi.dev/api/planets/" + id)
+                    const data = await response.json();
+                    console.log(data);
+                    // let propiedades = data.results;
+                    // console.log(propiedades);
+                    setStore({
+                        detallesPlaneta: data
+                    });
+
+
+                } catch (err) {
+                    console.log(err);
+                }
+
+            },
+
+            getDetalleVehiculo: async (id) => {
+
+                try {
+                    const response = await fetch("https://swapi.dev/api/vehicles/" + id)
+                    const data = await response.json();
+                    console.log(data);
+                    // let propiedades = data.results;
+                    // console.log(propiedades);
+                    setStore({
+                        detallesVehiculo: data
+                    });
+
+
+                } catch (err) {
+                    console.log(err);
+                }
+
+            },
 		}
 	};
 };
