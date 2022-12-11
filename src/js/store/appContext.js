08cd1b1f871967af7132126a6d86b4cfbe94e313
lauramagallanes <pasocenturiontours@gmail.com>
2022-12-11 13:39:07 -0300

    cambios en context

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -31,6 +31,10 @@ const injectContext = PassedComponent => {
 			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
 			 *
 			 **/
+			state.actions.getListaPersonajes();
+            state.actions.getListaPlanetas();
+            state.actions.getListaVehiculos();
+            state.actions.validToken();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
